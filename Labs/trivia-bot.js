9af3656b882cccdac3bc6ce67826fb336fe56521
Itır Emiroğlu <itiremiroglu@Itr-MacBook-Pro.local>
2026-06-25 11:36:04 +0300

    First Lab Exercise: JavaScript Certification

---
+++ Labs/trivia-bot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Sophie",
+  botLocation = "Cyprus",
+  favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact =
+  favoriteLanguage + " was created in just 10 days by Brendan Eich!";
+console.log(codingFact);
+codingFact = favoriteLanguage + " also called Vanilla JavaScript.";
+console.log(codingFact);
+codingFact =
+  "You can build front-end, back-end and mobile apps with " +
+  favoriteLanguage +
+  ".";
+console.log(codingFact);
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);
