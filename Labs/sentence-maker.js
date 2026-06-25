e2d6e50e7e1e7e68894b23ea02daa1e69915f398
Itır Emiroğlu <itiremiroglu@Itr-MacBook-Pro.local>
2026-06-26 00:26:43 +0300

    Sentence Maker

---
+++ Labs/sentence-maker.js
@@ -0,0 +1,46 @@
+let adjective = "curious",
+  noun = "detective",
+  verb = "excited",
+  place = "spaceship",
+  adjective2 = "tiny",
+  noun2 = "pizza";
+let firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("First story: " + firstStory);
+adjective = "clumsy";
+noun = "wizard";
+verb = "scared";
+place = "castle";
+adjective2 = "neon-green";
+noun2 = "waffles";
+let secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("Second story: " + secondStory);
