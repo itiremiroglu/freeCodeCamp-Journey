b08659703d8bb3b349b731560c76c008fc84886a
itiremiroglu <itiresenemiroglu@gmail.com>
2026-08-30 16:52:44 +0300

    Factorial Calculator

---
+++ Labs/factorial-calculator.js
@@ -0,0 +1,13 @@
+const num = 5;
+
+
+function factorialCalculator(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+const factorial = factorialCalculator(num);
+const resultMsg = `Factorial of ${num} is ${factorial}.`;
+console.log(resultMsg);
