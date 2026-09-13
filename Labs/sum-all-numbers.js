812665f5e30e0da88a9eaa509361833a41fd086b
itiremiroglu <itiresenemiroglu@gmail.com>
2026-09-14 01:05:40 +0300

    Design a Sum All Numbers Algorithms

---
+++ Labs/sum-all-numbers.js
@@ -0,0 +1,9 @@
+function sumAll(arr) {
+    let min = Math.min(arr[0], arr[1]);
+    let max = Math.max(arr[0], arr[1]);
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
+}
