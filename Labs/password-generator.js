46f02ced9772602fd2c93942709f1c83c808f734
itiremiroglu <itiresenemiroglu@gmail.com>
2026-09-14 00:29:35 +0300

    Password Generator App

---
+++ Labs/password-generator.js
@@ -0,0 +1,9 @@
+function generatePassword(length) {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * chars.length);
+        result += chars[randomIndex];
+    }
+    return result;
+}
